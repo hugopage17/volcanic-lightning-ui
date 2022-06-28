3ca81daba2085ed99cf8094bddfe7b9b8ef7cbc5
hugopage17 <hugopage0017@gmail.com>
2022-06-28 19:53:25 +1200

    fixe dmismatch unit tests

--- test/sorted-strikes.test.ts
+++ test/sorted-strikes.test.ts
@@ -2,8 +2,8 @@ import { Feature } from '@global-volcanic-lightning/types';
 import { highestStrikes, alertStrikes } from '../src/api/sortedStrikeData';
 import lightningMock from './lightning-mock.json';
 
-describe('Highest Striked Volcanoes', () => {
-    const strikes = alertStrikes(lightningMock.features as Feature[]);
+describe('Top 10 Highest Striked Volcanoes', () => {
+    const strikes = highestStrikes(lightningMock.features as Feature[]);
     const locations = strikes.map((strike) => strike.name)
     it("Highest striked volcanoes should only contain 'Kabargin Oth Group', 'Mono Craters', 'Inyo Craters'", () => {
         expect(locations).toEqual([
@@ -14,10 +14,10 @@ describe('Highest Striked Volcanoes', () => {
     })
 });
 
-describe('High Alert Volcanoes', () => {
-    const strikes = highestStrikes(lightningMock.features as Feature[]);
+describe('All High Alert Volcanoes', () => {
+    const strikes = alertStrikes(lightningMock.features as Feature[]);
     const locations = strikes.map((strike) => strike.name)
-    it("High alert volanoes should only contain 'Kuchinoerabu-jima', 'Kikai', 'Gaja-jima', 'Kogaja-jima', 'Almolonga', 'Santa María', 'Tolimán', 'Atitlán', 'Acatenango', 'Fuego'", () => {
+    it("High alert volcanoes should only contain 'Kuchinoerabu-jima', 'Kikai', 'Gaja-jima', 'Kogaja-jima', 'Almolonga', 'Santa María', 'Tolimán', 'Atitlán', 'Acatenango', 'Fuego'", () => {
       expect(locations).toEqual([
         'Kuchinoerabu-jima',
         'Kikai',
